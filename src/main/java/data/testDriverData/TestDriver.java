7f285321e199c1cdc2d3089fd5287e076db00a15
awesome0112 <nguyenminhquan01122003@gmail.com>
2024-11-20 01:56:03 +0700

    update sample project

--- src/main/java/data/testDriverData/TestDriver.java
+++ src/main/java/data/testDriverData/TestDriver.java
@@ -19,82 +19,41 @@ writer.close();
 e.printStackTrace();
 }
 }
-public static int getNumberOfDays(int month, int year)
+public static boolean isPerfectNumber(double num)
 {
-mark("int numOfDays=0;\n", false, false);
-int numOfDays=0;
-mark("int i=1", false, false);
-for (int i=1; ((i <= month) && mark("i <= month", true, false)) || mark("i <= month", false, true); mark("i++", false, false),
+if (((num <= 1) && mark("num <= 1", true, false)) || mark("num <= 1", false, true))
+{
+mark("return false;\n", false, false);
+return false;
+}
+mark("int sum=1;\n", false, false);
+int sum=1;
+mark("int i=2", false, false);
+for (int i=2; ((i <= Math.sqrt(num)) && mark("i <= Math.sqrt(num)", true, false)) || mark("i <= Math.sqrt(num)", false, true); mark("i++", false, false),
 i++) {
 {
-if (((i == 2) && mark("i == 2", true, false)) || mark("i == 2", false, true))
+if (((num % i == 0) && mark("num % i == 0", true, false)) || mark("num % i == 0", false, true))
 {
 {
-if (((isLeapYear(year)) && mark("isLeapYear(year)", true, false)) || mark("isLeapYear(year)", false, true))
+mark("sum+=i;\n", false, false);
+sum+=i;
+if (((i != num / i) && mark("i != num / i", true, false)) || mark("i != num / i", false, true))
 {
-{
-mark("numOfDays+=29;\n", false, false);
-numOfDays+=29;
-}
-}
-else {
-mark("numOfDays+=28;\n", false, false);
-numOfDays+=28;
-}
-}
-}
-else {
-if (((i <= 7) && mark("i <= 7", true, false)) || mark("i <= 7", false, true))
-{
-{
-if (((i % 2 == 0) && mark("i % 2 == 0", true, false)) || mark("i % 2 == 0", false, true))
-{
-mark("numOfDays+=30;\n", false, false);
-numOfDays+=30;
-}
-else {
-mark("numOfDays+=31;\n", false, false);
-numOfDays+=31;
-}
-}
-}
-else {
-{
-if (((i % 2 == 0) && mark("i % 2 == 0", true, false)) || mark("i % 2 == 0", false, true))
-{
-mark("numOfDays+=31;\n", false, false);
-numOfDays+=31;
-}
-else {
-mark("numOfDays+=30;\n", false, false);
-numOfDays+=30;
+mark("sum+=num / i;\n", false, false);
+sum+=num / i;
 }
 }
 }
 }
 }
-}
-mark("return numOfDays;\n", false, false);
-return numOfDays;
+mark("return sum == num;\n", false, false);
+return sum == num;
 }
 
-
-public static boolean isLeapYear(int year){
-  if (year % 4 == 0) {
-    if (year % 100 == 0) {
-      if (year % 400 == 0)       return true;
- else       return false;
-    }
- else {
-      return true;
-    }
-  }
-  return false;
-}
 public static void main(String[] args) {
 writeDataToFile("", "src/main/java/data/testDriverData/runTestDriverData.txt", false);
 long startRunTestTime = System.nanoTime();
-Object output = getNumberOfDays(8, 8);
+Object output = isPerfectNumber(0.0);
 long endRunTestTime = System.nanoTime();
 double runTestDuration = (endRunTestTime - startRunTestTime) / 1000000.0;
 writeDataToFile(runTestDuration + "===" + output, "src/main/java/data/testDriverData/runTestDriverData.txt", true);
